ee0f8dac5944a3a882af0788b882080c5d5f8eaa
Xinrea <xinreas@outlook.com>
2025-02-16 13:20:45 +0800

    feat: display response content of qrcode error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,24 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: '@babel/eslint-parser',
+    parser: '@babel/eslint-parser'
   },
   env: {
     browser: true,
     node: true
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:vue/recommended"
-  ],
+  extends: ['eslint:recommended', 'plugin:vue/recommended'],
   globals: {
     __static: true
   },
-  plugins: ['html','vue'],
+  plugins: ['html', 'vue'],
   rules: {
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'vue/html-self-closing': 0
   }
 }
